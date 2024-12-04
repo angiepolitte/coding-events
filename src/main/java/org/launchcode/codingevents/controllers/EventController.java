ccbe2f770ba815f09aeb2cbd982aa9ed27d696e2
Angie <angiepolitte@gmail.com>
2024-12-04 08:58:51 -0600

    Add events from browser to display a newly created list

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,24 +3,21 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Berra Christmas");
-        events.add("Politte Christmas");
-        events.add("Christmas Concert");
-        events.add("Christmas Eve");
-        events.add("Christmas Day");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -29,5 +26,13 @@ public class EventController {
     public String renderCreateEventForm () {
         return "events/create";
     }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+           events.add(eventName);
+           return "redirect:/events";
+    }
+
+
 
 }
